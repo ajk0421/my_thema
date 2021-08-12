0a132c69c0f982093917c790d264fe1aa85e7331
ajk0421 <ajk.spirituallounge@gmai.com>
2021-08-12 18:21:53 +0900

    sass 導入

--- my_thema/functions.php
+++ my_thema/functions.php
@@ -1,11 +1,11 @@
 <?php
-  function register_stylesheet() {
-    wp_register_style('style', get_stylesheet_directory_uri().'/style.css');
-  }
-
   function add_stylesheet() {
-    register_stylesheet();
+    wp_register_style('style', get_stylesheet_directory_uri().'/style.css');
     wp_enqueue_style('style', '', array(), '1.0', false);
+
+    wp_register_style('sass-style', get_stylesheet_directory_uri().'/sass/style.css');
+    wp_enqueue_style('sass-style', '', array(), '1.0', false);
+
   }
   add_action('wp_enqueue_scripts', 'add_stylesheet');
 ?>
