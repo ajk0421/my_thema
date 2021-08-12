91bfb7afc3dd60c6a18147bfc102ad8722df73ef
ajk0421 <ajk.spirituallounge@gmai.com>
2021-08-12 19:43:29 +0900

    jquery 導入

--- my_thema/functions.php
+++ my_thema/functions.php
@@ -1,11 +1,16 @@
 <?php
-  function add_stylesheet() {
-    wp_register_style('style', get_stylesheet_directory_uri().'/style.css');
-    wp_enqueue_style('style', '', array(), '1.0', false);
 
-    wp_register_style('sass-style', get_stylesheet_directory_uri().'/sass/style.css');
-    wp_enqueue_style('sass-style', '', array(), '1.0', false);
+  // CSS読み込み
+  function add_stylesheet() {
+    wp_enqueue_style('style', get_stylesheet_directory_uri().'/style.css', array(), '1.0', false);
+    wp_enqueue_style('sass-style', get_stylesheet_directory_uri().'/sass/style.css', array(), '1.0', false);
 
   }
   add_action('wp_enqueue_scripts', 'add_stylesheet');
+
+  // JS読み込み
+  function add_javascripts() {
+	  wp_enqueue_script('application', get_stylesheet_directory_uri().'/javascripts/application.js');
+  }
+  add_action( 'wp_enqueue_scripts', 'add_javascripts' );
 ?>
